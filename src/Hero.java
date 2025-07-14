c345e56dc84f663ea5f623005e2bd736d6bb5348
NishimuraTaiki <241023@std.hi-joho.ac.jp>
2025-07-14 11:13:56 +0900

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,18 @@
+class Hero extends Character {
+    private String weapon;
+    public Hero(String name,int hp,String weapon) {
+        super(name,hp);
+    }
+    public void attack(Character target) {
+        System.out.println(getName() + "は"+ getWeapon() + "で攻撃！" + target.getName() + "に10のダメージを与えた!");
+        target.setHp(target.getHp()-10);
+    }
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+}
